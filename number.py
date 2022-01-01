2625df2c51b727f51204f5883bf02de369cd8e7e
Edison <edison448@gmail.com>
2022-01-01 10:59:12 +0800

    2nd version

--- number.py
+++ number.py
@@ -1,14 +1,17 @@
 import random
 ans = random.randint(1, 100)
-
+count = 0
 
 while True:
+	count = count + 1
 	num = input('pls type number')
 	num = int(num)
 	if num == ans:
-		print('pass')
+		print('pass', )
+		print('count number', count)
 		break
 	elif num > ans:
 		print('比較大')
 	elif num < ans:
 		print('比較小')
+	print('count number', count)
